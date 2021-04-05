4c32a9e9a0eddc28012c63017ba5bcda4273f8db
Lâm <tdlam123@gmail.com>
2021-04-05 19:02:37 +0700

    update

--- AI_NangCao/DoAnCaiDat/Project/config.py
+++ AI_NangCao/DoAnCaiDat/Project/config.py
@@ -5,3 +5,5 @@ TRAIN_PATH = INPUT + '/Train'
 TEST_PATH = INPUT + '/Test'
 
 STANDARD_SIZE = (50, 50)
+
+MODEL_PATH = 'model/tdlam_Model_1st.h5'
--- AI_NangCao/DoAnCaiDat/Project/tester_subclass.py
+++ AI_NangCao/DoAnCaiDat/Project/tester_subclass.py
@@ -7,33 +7,19 @@ import numpy as np
 from config import TEST_PATH, STANDARD_SIZE, MODEL_PATH
 
 
-def test():
+# Test trên tập test ban đầu, có lọc ra tập test là loại con cụ thể trong 6 loại để so sánh
+def test_subclass():
     model = keras.models.load_model(MODEL_PATH)
-    # Test trên tập test ban đầu
     test_img = sorted(os.listdir(TEST_PATH))
+    # todo filter các hình thuộc loại cần để test
     import image_utils
     from sklearn.metrics import classification_report
 
     test_images = image_utils.scale(test_img, test_path=TEST_PATH)
     test = pd.read_csv('./input/Test.csv')
     y_test = test['ClassId'].values
+    # todo filter các hình thuộc loại cần để test
     print(y_test)
     y_pred = model.predict_classes(test_images)
     print(y_pred)
     print(classification_report(y_test, y_pred))
-
-
-def single_test(image_path):
-    model = keras.models.load_model(MODEL_PATH)
-    img = Image.open(image_path)
-    print(np.array(img).shape)
-    img = img.resize(STANDARD_SIZE)
-    img = np.array(img)
-    images = np.array([img])
-    images = images / 255
-    print(model.predict_classes(images))
-
-
-test()
-single_test('./input/random.png')
-single_test('./input/out.png')
